5abff571093d950ecb0b40f2dc102273204cf7ae
Andrew Frederick <ajf39@uw.edu>
2025-10-23 23:00:25 -0700

    Done!

--- lab02/js/main.js
+++ lab02/js/main.js
@@ -51,3 +51,19 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+function openTab(evt, tabName) {
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  const tablinks = document.getElementsByClassName("tablinks");
+
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.classList.add("active");
+}
